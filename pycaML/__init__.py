9b1211eaf4090a93b392f43347866bdfafc5aab2
Donato Riccio <donato.riccio@gmail.com>
2022-10-08 16:35:26 +0200

    version 0.0.3
    - bug fixes
    - cleaned the code with flake8

--- pycaML/__init__.py
+++ pycaML/__init__.py
@@ -1,2 +1,1 @@
 from .experiments import RegressionExperiment, BinaryClassificationExperiment, MulticlassExperiment
-from .models import models_class, models_reg, models_stacking_class, models_stacking_reg
